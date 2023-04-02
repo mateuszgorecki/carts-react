c02193ff3a8b2c6bb0e9e435ba6c35757b3c9451
mateusz <mateusz.gorecki.dev@gmail.com>
2023-04-02 17:03:24 +0200

    done styling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,25 +1,22 @@
 import { isRouteErrorResponse, useRouteError, NavLink } from 'react-router-dom'
-
 import Header from '../components/Header'
+
+import styles from '../styles/Error.module.scss'
 
 function ErrorPage() {
   const error = useRouteError()
   console.log(error)
 
   if (isRouteErrorResponse(error)) {
-    if (error.status === 404) {
-      console.log('Page not found', error)
-    }
-
     return (
       <>
         <Header />
-        <main id='error-content'>
+        <main id='error-content' className={styles.error}>
           <h1>Oops, {error.status}!</h1>
           <p>{error.statusText}</p>
           {error.data?.message && (
             <p>
-              <i>{error.data.message}</i>
+              <i className={styles.message}>"{error.data.message}"</i>
             </p>
           )}
         </main>
@@ -27,17 +24,19 @@ function ErrorPage() {
     )
   } else if (error instanceof Error) {
     return (
-      <div id='error-page'>
+      <div id='error-page' className={styles.error}>
         <h1>Oops! Unexpected Error</h1>
         <p>Something went wrong.</p>
         <p>
-          <i>{error.message}</i>
+          <i className={styles.message}>"{error.message}"</i>
         </p>
+        <p>Take me home.</p>
+        <NavLink to='/'>Home</NavLink>
       </div>
     )
   } else
     return (
-      <div id='error-page'>
+      <div id='error-page' className={styles.error}>
         <h1>Oops! Page not found.</h1>
         <p>Take me home.</p>
         <NavLink to='/'>Home</NavLink>
